19386dfeeaa5851691633af4bb73222c2309e6cc
Kavyasri-S2006 <ikavyasri22@gmail.com>
2026-02-18 21:16:39 +0530

    Time: 3 ms (87.57%) | Memory: 43.2 MB (86.41%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+      Stack<Character> stack=new Stack<>();
+    int len=s.length();
+    for(int i=0;i<len;i++){
+        char ch=s.charAt(i);
+        if(ch=='{'||ch=='['||ch=='('){
+            stack.push(ch);
+        }
+        else{
+            if(stack.isEmpty()) return false;
+            else{
+                char popch=stack.pop();
+                if((ch==')'&& popch!='(') ||
+                    (ch==']' && popch!='[') ||
+                    (ch=='}' && popch!='{')) {
+                        return false;
+                    }
+            }
+        }
+    }
+    return stack.isEmpty();
+    }
+}
