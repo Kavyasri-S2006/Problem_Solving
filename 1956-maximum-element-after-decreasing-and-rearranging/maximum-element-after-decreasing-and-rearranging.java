1df529a59865cf268a16be2445ae4ba74037ce36
Kavyasri-S2006 <ikavyasri22@gmail.com>
2026-06-24 11:27:58 +0530

    Time: 12 ms (7.22%) | Memory: 77.5 MB (39.69%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(arr);
         if(arr[0]!=1)arr[0]=1;
         for(int i=1;i<arr.length;i++){
-            if(arr[i]-arr[i-1]>1) arr[i]=arr[i-1]+1;
+            if(Math.abs(arr[i]-arr[i-1])>1) arr[i]=arr[i-1]+1;
         }
         int max=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
