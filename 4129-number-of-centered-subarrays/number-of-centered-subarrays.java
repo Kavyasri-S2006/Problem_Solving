9929e356c5d99be1141cfe62fbb7ea58323efd6d
Kavyasri-S2006 <ikavyasri22@gmail.com>
2026-07-01 11:45:08 +0530

    Time: 101 ms (22.81%) | Memory: 46.9 MB (49.71%) - LeetSync

--- 4129-number-of-centered-subarrays/number-of-centered-subarrays.java
+++ 4129-number-of-centered-subarrays/number-of-centered-subarrays.java
@@ -1,16 +1,27 @@
 class Solution {
     public int centeredSubarrays(int[] nums) {
-        int c=0;
-        List<Integer> list=new ArrayList<>();
-        for(int n:nums)list.add(n);
-        for(int i=0;i<nums.length;i++){
-            int sum=0; 
-            for(int j=i;j<nums.length;j++){
-                list.add(nums[j]);
-                sum+=nums[j];
-                if(list.subList(i,j+1).contains(sum))c++;
+            int c=0;
+            for(int i=0;i<nums.length;i++){
+                Set<Integer> set =new HashSet<>();
+                int sum=0;
+                for(int j=i;j<nums.length;j++){
+                    set.add(nums[j]);
+                    sum+=nums[j];
+                    if(set.contains(sum))c++;
+                }
             }
-        }
-        return c;
+            return c;
+        // int c=0;
+        // List<Integer> list=new ArrayList<>();
+        // for(int n:nums)list.add(n);
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=0; 
+        //     for(int j=i;j<nums.length;j++){
+        //         list.add(nums[j]);
+        //         sum+=nums[j];
+        //         if(list.subList(i,j+1).contains(sum))c++;
+        //     }
+        // }
+        // return c;
     }
 }
