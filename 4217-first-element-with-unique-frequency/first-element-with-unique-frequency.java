e3d9757bd1ea67f770857073d5740ff24f5320ae
Kavyasri-S2006 <ikavyasri22@gmail.com>
2026-07-02 10:48:43 +0530

    Time: 118 ms (58.44%) | Memory: 214.6 MB (76.19%) - LeetSync

--- 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.java
+++ 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.java
@@ -1,7 +1,7 @@
 class Solution {
     public int firstUniqueFreq(int[] nums) {
       Map<Integer,Integer> map=new HashMap<>();
-      List<Integer> list=new ArrayList<>();
+      
       for(int n:nums){
        map.put(n,map.getOrDefault(n,0)+1);
       }
