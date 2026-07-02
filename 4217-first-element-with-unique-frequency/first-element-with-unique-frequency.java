c48f54580b6115f24e9c8813a6572feefc2aa000
Kavyasri-S2006 <ikavyasri22@gmail.com>
2026-07-02 10:59:48 +0530

    Time: 112 ms (87.66%) | Memory: 225.8 MB (22.08%) - LeetSync

--- 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.java
+++ 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.java
@@ -1,18 +1,37 @@
 class Solution {
     public int firstUniqueFreq(int[] nums) {
-      Map<Integer,Integer> map=new HashMap<>();
+      Map<Integer,Integer> map=new LinkedHashMap<>();
       
       for(int n:nums){
        map.put(n,map.getOrDefault(n,0)+1);
       }
-    Map<Integer,Integer> m=new HashMap<>();
+    Map<Integer,Integer> m=new LinkedHashMap<>();
     for(int n:map.values()){
         m.put(n,m.getOrDefault(n,0)+1);
     }
-    for(int n:nums){
-        int freq=map.get(n);
-        if(m.get(freq)==1) return n ;
-    }
+    int tar=0;
+    for(Map.Entry<Integer,Integer> e:m.entrySet()){
+         if(e.getValue()==1){
+             tar=e.getKey();
+              break; 
+              } 
+        }
+         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+             if(entry.getValue()==tar)return entry.getKey();
+              }
+    // for(Map.Entry<Integer,Integer> e:m.entrySet()){
+    //     if(e.getValue()==1){
+    //         tar=e.getKey();
+    //         break;
+    //     }
+    // }
+    // for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+    //     if(entry.getValue()==tar) return map.entrySet();
+    // }
+    // for(int n:nums){
+    //     int freq=map.get(n);
+    //     if(m.get(freq)==1) return n ;
+    // }
     return -1;
     }
 }
