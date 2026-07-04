4fe7cdcde9f26a1c794402b53e921cd2b3e538b1
Kavyasri-S2006 <ikavyasri22@gmail.com>
2026-07-04 12:25:02 +0530

    Time: 52 ms (96.08%) | Memory: 105.6 MB (48.90%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
@@ -1,22 +1,36 @@
 class Solution {
     public int countDistinctIntegers(int[] nums) {
-   int count=1;
-    List<Integer> list=new ArrayList<>(); 
-    for(int n:nums)list.add(n);
-    
-    for(int n:nums){
-        int rev=0;
-        while(n!=0){
-            int rem=n%10;
-            rev=rev*10+rem;
-            n/=10;
-        }
-       list.add(rev);
-    } 
-    Collections.sort(list);
-    for(int i=1;i<list.size();i++){
-        if(list.get(i)>list.get(i-1))count++;
+
+   Set<Integer> set=new HashSet<>();
+   for(int n:nums)set.add(n);
+   for(int n:nums){
+    int rev=0;
+    while(n!=0){
+        int rem=n%10;
+        rev=rev*10+rem;
+        n/=10;
     }
-    return count;
+    set.add(rev);
+   }
+   return set.size();
+
+    // int count=1;
+    // List<Integer> list=new ArrayList<>(); 
+    // for(int n:nums)list.add(n);
+    
+    // for(int n:nums){
+    //     int rev=0;
+    //     while(n!=0){
+    //         int rem=n%10;
+    //         rev=rev*10+rem;
+    //         n/=10;
+    //     }
+    //    list.add(rev);
+    // } 
+    // Collections.sort(list);
+    // for(int i=1;i<list.size();i++){
+    //     if(list.get(i)>list.get(i-1))count++;
+    // }
+    //return count;
     }
 }
