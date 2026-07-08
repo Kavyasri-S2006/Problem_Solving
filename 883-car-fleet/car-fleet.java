1348b1a8da99996bc564d22441e300963294902f
Kavyasri-S2006 <ikavyasri22@gmail.com>
2026-07-08 14:10:52 +0530

    Time: 61 ms (96.05%) | Memory: 117.1 MB (7.56%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        double max=Integer.MIN_VALUE;
+        int count=0;
+        int n=position.length;
+        Map<Integer,Integer> map=new LinkedHashMap<>();
+      for(int i=0;i<n;i++){
+        map.put(position[i],speed[i]);
+      } 
+      Arrays.sort(position);
+      for(int i= position.length-1;i>=0;i--){
+        double dis=target-position[i];
+        double sp=map.get(position[i]);
+            double t=(double)(dis/sp);
+            if(t>max){
+                count++;
+                max=t;
+            }    
+        } 
+        return count;
+    }
+}
