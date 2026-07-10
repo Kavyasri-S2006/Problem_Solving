e48807ae527712078c73b39cc98792e60531e846
Kavyasri-S2006 <ikavyasri22@gmail.com>
2026-07-10 22:46:11 +0530

    Time: 4 ms (6.25%) | Memory: 48.5 MB (5.26%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -1,70 +1,74 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-//         int n=nums.length;
-//         List<String> list=new ArrayList<>();
-//         if(nums.length==0 || nums.length==1)return list;
-//         Stack<Integer> stack=new Stack<>();
-//         stack.push(nums[0]);
-//         for(int i=1;i<n;i++){
-//             int start=0;
-//             int end=0;
-//             if(!stack.isEmpty() && stack.peek()+1==nums[i]){
-//                 stack.push(nums[i]);
-//                 if(i==n-1){
-//                     end=stack.pop();
-//                     while(!stack.isEmpty()) start=stack.pop();
-//                     list.add(String.valueOf(start)+"->"+String.valueOf(end));
-//                 }
-                    
-//             }else{
-//                 if(!stack.isEmpty()){
-//                     end=stack.pop();
-//                     if(stack.isEmpty())list.add(String.valueOf(end));
-//                     else{
-//                         while(!stack.isEmpty()){
-//                         start=stack.pop();
-//                     }
-//                     list.add(String.valueOf(start)+"->"+String.valueOf(end));
-//                     }
-//                 }
-//             if(stack.isEmpty() && i==n-1 )list.add( String.valueOf(nums[i]));
-//             stack.push(nums[i]);
-//             }
-//         }
-//         return list;
-//     }
-// }
         int n=nums.length;
         List<String> list=new ArrayList<>();
         if(nums.length==0 )return list;
-       int start=nums[0];
         if(nums.length==1){
-            list.add(String.valueOf(start));
+            list.add(String.valueOf(String.valueOf(nums[0])));
             return list;
         }
-        int end=nums[0];
-         int count=1;
-       
+        Stack<Integer> stack=new Stack<>();
+        stack.push(nums[0]);
         for(int i=1;i<n;i++){
-            //int count=1
-            if(nums[i]==end+1){
-                end=nums[i];
-                count++;
-                if(i==nums.length-1)list.add(String.valueOf(start)+"->"+String.valueOf(end));
-            }
-            else {
-            if (count!=1) list.add(String.valueOf(start)+"->"+String.valueOf(end));
-            if(count==1)list.add(String.valueOf(start));
-            start=nums[i];
-            end=nums[i];
-            count=1;
-            if(i==nums.length-1 ){
-                    if(start==end && count==1)list.add(String.valueOf(start));
+            int start=0;
+            int end=0;
+            if(!stack.isEmpty() && stack.peek()+1==nums[i]){
+                stack.push(nums[i]);
+                if(i==n-1){
+                    end=stack.pop();
+                    while(!stack.isEmpty()) start=stack.pop();
+                    list.add(String.valueOf(start)+"->"+String.valueOf(end));
                 }
-                
+                    
+            }else{
+                if(!stack.isEmpty()){
+                    end=stack.pop();
+                    if(stack.isEmpty())list.add(String.valueOf(end));
+                    else{
+                        while(!stack.isEmpty()){
+                        start=stack.pop();
+                    }
+                    list.add(String.valueOf(start)+"->"+String.valueOf(end));
+                    }
+                }
+            if(stack.isEmpty() && i==n-1 )list.add( String.valueOf(nums[i]));
+            stack.push(nums[i]);
+            }
         }
-       
-    }
-     return list;
+        return list;
     }
 }
+//         int n=nums.length;
+//         List<String> list=new ArrayList<>();
+//         if(nums.length==0 )return list;
+//        int start=nums[0];
+//         if(nums.length==1){
+//             list.add(String.valueOf(start));
+//             return list;
+//         }
+//         int end=nums[0];
+//          int count=1;
+       
+//         for(int i=1;i<n;i++){
+//             //int count=1
+//             if(nums[i]==end+1){
+//                 end=nums[i];
+//                 count++;
+//                 if(i==nums.length-1)list.add(String.valueOf(start)+"->"+String.valueOf(end));
+//             }
+//             else {
+//             if (count!=1) list.add(String.valueOf(start)+"->"+String.valueOf(end));
+//             if(count==1)list.add(String.valueOf(start));
+//             start=nums[i];
+//             end=nums[i];
+//             count=1;
+//             if(i==nums.length-1 ){
+//                     if(start==end && count==1)list.add(String.valueOf(start));
+//                 }
+                
+//         }
+       
+//     }
+//      return list;
+//     }
+// }
