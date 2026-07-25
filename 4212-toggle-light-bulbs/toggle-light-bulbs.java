9b496a44f4e84881261455d4f20a44c709f5562e
Kavyasri-S2006 <ikavyasri22@gmail.com>
2026-07-25 20:27:43 +0530

    Time: 2 ms (99.87%) | Memory: 46.6 MB (90.71%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.java
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.java
@@ -1,15 +1,20 @@
 class Solution {
     public List<Integer> toggleLightBulbs(List<Integer> bulbs) {
         List<Integer> list=new ArrayList<>();
-        
-        Map<Integer,Integer> map=new TreeMap<>();
-        for(int i=0;i<bulbs.size();i++){
-            int x=bulbs.get(i);
-            map.put(x,map.getOrDefault(x,0)+1);
-        }
-        for(Map.Entry<Integer,Integer> en:map.entrySet()){
-            if(en.getValue()%2!=0)list.add(en.getKey());
+        int[] freq=new int[101];
+        for(int i=0;i<bulbs.size();i++)freq[bulbs.get(i)]++;
+        for(int i=0;i<freq.length;i++){
+            if( freq[i] %2  != 0)list.add(i);
         }
         return list;
+        // Map<Integer,Integer> map=new TreeMap<>();
+        // for(int i=0;i<bulbs.size();i++){
+        //     int x=bulbs.get(i);
+        //     map.put(x,map.getOrDefault(x,0)+1);
+        // }
+        // for(Map.Entry<Integer,Integer> en:map.entrySet()){
+        //     if(en.getValue()%2!=0)list.add(en.getKey());
+        // }
+        // return list;
     }
 }
